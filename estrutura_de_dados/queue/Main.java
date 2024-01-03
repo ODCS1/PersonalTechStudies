7ba77b390ea8c96cb3d5d966a212250d00096454
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-01-03 09:36:26 -0300

    update

---
+++ estrutura_de_dados/queue/Main.java
@@ -0,0 +1,15 @@
+package queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+// Queue/Fila = FIFO (FIRST IN FIRST OUT)
+
+// enqueue (add elements) --> offer()
+// dequeue (remove elements) --> poll()
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<String>();
+    }
+}
---
+++ estrutura_de_dados/stack/Main.java
@@ -0,0 +1,29 @@
+package stack;
+
+import java.util.Stack;
+
+// Stack ou pilha é a estrutura LIFO (Last in first out)
+
+// Adicona os elementos no topo de uma uma pilha vertival
+
+// push() adiciona ao top
+// pop() remove do top
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Laranja");
+        stack.push("Melancia");
+        stack.push("Melão");
+
+        // String fruta = stack.pop();
+
+        // stack.push(fruta);
+
+        System.out.println(stack.search("Melão"));
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
+    }
+}
