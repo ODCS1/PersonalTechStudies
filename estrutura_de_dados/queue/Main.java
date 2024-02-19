307e624044da9c50073758b43902269e579f362c
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-02-19 14:15:26 -0300

    Update Main.java

--- estrutura_de_dados/queue/Main.java
+++ estrutura_de_dados/queue/Main.java
@@ -11,5 +11,10 @@ import java.util.Queue;
 public class Main {
     public static void main(String[] args) {
         Queue<String> queue = new LinkedList<String>();
+        queue.offer("Teste");
+        queue.offer("Mais outro");
+        queue.poll();
+
+        System.out.println(queue);
     }
 }
