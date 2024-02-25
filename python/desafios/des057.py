bfdf76cec2d9bed5e2e25899548362c1d52d7965
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-02-25 19:31:30 -0300

    update type hints

--- python/desafios/des057.py
+++ python/desafios/des057.py
@@ -1,7 +1,7 @@
 # Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 while True:
-    s = input("Digite: ")
+    s: str = input("Digite: ")
 
     if (s in ["M", "F"]):
         print("SAINDO...")
---
+++ python/praticando/p011/script63.py
@@ -0,0 +1,5 @@
+def typeHints (num: int) -> str:
+    return f"{num + 10}" 
+
+qualTipo: str = typeHints(10)
+print(qualTipo)
