b9a831154985c6a696d829d5a96341e38cdb66ac
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-02-26 21:08:55 -0300

    up codigo

---
+++ csharp/data-structures/apHashSimples/HashSimples.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class HashSimples
+    {
+        const int tamanhoPadrao = 10007;
+        string[] tabelaDeHash;
+
+
+        public HashSimples(int tamanhoDesejado)
+        {
+        tabelaDeHash = new string[tamanhoDesejado];
+        }
+
+        public HashSimples(): this(tamanhoPadrao) {}
+
+        private int Hash(string chave)
+        {
+            int tot = 0;
+            for (int i = 0; i < chave.Length; i++)
+                tot += (int)chave[i];
+            return tot % tabelaDeHash.Length;
+        }
+
+}
+
