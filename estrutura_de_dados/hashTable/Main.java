858acb9e7025cfba94d93d4a5f7663cbeed3bccf
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-03-11 22:50:12 -0300

    Update Main.java

--- estrutura_de_dados/hashTable/Main.java
+++ estrutura_de_dados/hashTable/Main.java
@@ -1,7 +1,21 @@
 package hashTable;
 
+import java.util.Hashtable;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Hashtable<Integer, String> tb = new Hashtable<>(2);
+
+
+        // tb.put(0, "Primeiro");
+
+        for (int i = 0; i < 10; i++) {
+            tb.put(i, "Valor " + Integer.toString(i));
+            // System.out.println(i);
+        }
+ 
+        tb.put(10, "Valor");
+
+        System.out.println(tb);
     }
 }
