9d8eed4979a2b3d93253dad5af98694d190ad406
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-03-13 20:39:05 -0300

    Update main.py

--- python/travel-salesmen/main.py
+++ python/travel-salesmen/main.py
@@ -1,5 +1,6 @@
-n_cities = 17
+from random import choice
 
+n_cities = 17
 
 # MATRIZ DE DISTÂNCIAS
 distances = [
@@ -32,19 +33,25 @@ def get_total_distance(tour):
 
     total_distance += distances[tour[-1]][tour[0]]
 
-
+    return total_distance
 
 # EURÍSTICA DO VIZINHO MAIS PRÓXIMO
-def nearest_neighbor_heuristic():
-
+def random_nearest_neighbor_heuristic():
+    
     # AS CIDADES QUE AINDA NÃO FORAM VISITADAS
-    unvisited = list(1, range(n_cities))
-
-    tour = [0]
+    unvisited = list(range(0, n_cities))
+    tour = [choice(unvisited)]
+    # print(id(tour))
+    unvisited.remove(tour[0])
 
     while unvisited:
         
         next_city = min(unvisited, key= lambda city: distances[tour[-1]][city])
+        tour.append(next_city)
+        unvisited.remove(next_city)
 
-        
-
+    return tour
+# print(type(random_nearest_neighbor_heuristic()))
+tour = random_nearest_neighbor_heuristic()
+# print(tour)
+print(tour, get_total_distance(tour))
