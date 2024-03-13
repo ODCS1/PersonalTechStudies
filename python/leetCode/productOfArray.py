9d66c52557d3a5c7e4008968485f2920d32c9474
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-03-13 14:24:18 -0300

    Create productOfArray.py

---
+++ python/leetCode/productOfArray.py
@@ -0,0 +1,15 @@
+# 238. Product of Array Except Self
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+
+
+        pass
+
+
+obj = Solution()
+vet = [1, 2, 5, -1]
+
+print(f"Result: {obj.productExceptSelf(vet)}")
