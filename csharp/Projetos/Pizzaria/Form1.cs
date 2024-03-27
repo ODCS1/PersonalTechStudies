220c05723cb7d1dcfd86af24f7cfb769ac2ca6ce
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-03-27 13:19:35 -0300

    update Tela pizzaria

--- csharp/Projetos/Pizzaria/Form1.cs
+++ csharp/Projetos/Pizzaria/Form1.cs
@@ -36,5 +36,40 @@ namespace Pizzaria
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
