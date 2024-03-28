0a86a946074171c8be48099ad4fd6943ee88e823
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-03-27 22:53:14 -0300

    update

---
+++ csharp/Projetos/wfPizza/frmPizza.cs
@@ -0,0 +1,20 @@
+namespace wfPizza
+{
+    public partial class frmPedido : Form
+    {
+        public frmPedido()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
