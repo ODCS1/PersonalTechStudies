9314eceeb63311bc5849ee41c2eaa447f7c46cf6
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-04-18 15:08:30 -0300

    Update maxProductSubarray.py

--- python/leetCode/maxProductSubarray.py
+++ python/leetCode/maxProductSubarray.py
@@ -1,21 +1,45 @@
 from typing import List
 
+# Dynamic Programming
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        maxProduct = nums[0]
-        currentProduct = 1
+        result = max(nums)
+        maxCurrrentProduct, minCurrentProduct = 1, 1
 
-        for i in range(len(nums)):
-            if currentProduct < 0:
-                currentProduct = 0
+        for n in nums:
+            if n == 0:
+                minCurrentProduct, maxCurrrentProduct = 1, 1
+                continue
 
-            currentProduct *= nums[i]
-            maxProduct = max(maxProduct, currentProduct)
-        
-        return maxProduct
+            maxCurrrentProduct = max(maxCurrrentProduct * n, minCurrentProduct * n, n)  
+            minCurrentProduct = min(maxCurrrentProduct * n, minCurrentProduct * n, n)  
+
+            result = max(maxCurrrentProduct, result)
+
+
+        return result
+
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         result = max(nums)
+#         minCurrentProduct, maxCurrentProduct = 1, 1
+
+#         for n in nums:
+#             if n == 0:
+#                 minCurrentProduct, maxCurrentProduct = 1, 1
+#                 continue
+            
+#             maxCurrentProduct = max(maxCurrentProduct * n, minCurrentProduct * n, n)
+#             minCurrentProduct = min(maxCurrentProduct * n, minCurrentProduct * n, n)
+
+#             result = max(maxCurrentProduct, result)
+
+#         return result
     
 
 # vet = [-3,-1,-1]
-vet = [-2,0,-1]
+vet = [-4,-3,-2]
+# vet = [-2,0,-1, 2]
 obj = Solution()
 print(obj.maxProduct(vet))
