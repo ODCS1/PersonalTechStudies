fef4345ec850deb0696a70a0ef0ad2e93b349fd4
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-04-19 17:11:17 -0300

    Create finMinInSorArr.py

---
+++ python/leetCode/finMinInSorArr.py
@@ -0,0 +1,14 @@
+# 153. Find Minimum in Rotated Sorted Array
+
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        pass
+
+
+
+vet = [3,4,5,1,2]
+obj = Solution()
+print(obj.findMin(vet))
