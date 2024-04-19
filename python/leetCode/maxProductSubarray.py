4b0e45179d13c442af7ed694df2a33e4208ec382
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-04-19 15:29:39 -0300

    Update maxProductSubarray.py

--- python/leetCode/maxProductSubarray.py
+++ python/leetCode/maxProductSubarray.py
@@ -11,35 +11,17 @@ class Solution:
             if n == 0:
                 minCurrentProduct, maxCurrrentProduct = 1, 1
                 continue
+            
+            aux = maxCurrrentProduct * n
+            maxCurrrentProduct = max(n * maxCurrrentProduct, n * minCurrentProduct, n)  
+            minCurrentProduct = min(aux, n * minCurrentProduct, n)  
 
-            maxCurrrentProduct = max(maxCurrrentProduct * n, minCurrentProduct * n, n)  
-            minCurrentProduct = min(maxCurrrentProduct * n, minCurrentProduct * n, n)  
-
-            result = max(maxCurrrentProduct, result)
+            result = max(result, maxCurrrentProduct)
 
 
         return result
-
-# class Solution:
-#     def maxProduct(self, nums: List[int]) -> int:
-#         result = max(nums)
-#         minCurrentProduct, maxCurrentProduct = 1, 1
-
-#         for n in nums:
-#             if n == 0:
-#                 minCurrentProduct, maxCurrentProduct = 1, 1
-#                 continue
-            
-#             maxCurrentProduct = max(maxCurrentProduct * n, minCurrentProduct * n, n)
-#             minCurrentProduct = min(maxCurrentProduct * n, minCurrentProduct * n, n)
-
-#             result = max(maxCurrentProduct, result)
-
-#         return result
     
 
-# vet = [-3,-1,-1]
 vet = [-4,-3,-2]
-# vet = [-2,0,-1, 2]
 obj = Solution()
 print(obj.maxProduct(vet))
