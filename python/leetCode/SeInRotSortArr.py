1b14d87c4c65f433bec4680be301d687ff8d95b8
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-04-20 12:11:01 -0300

    Create SeInRotSortArr.py

---
+++ python/leetCode/SeInRotSortArr.py
@@ -0,0 +1,15 @@
+# 33. Search in Rotated Sorted Array
+
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+       
+       pass
+
+
+
+arr = [4,5,6,7,0,1,2]
+target = 2
+obj = Solution()
+print(obj.search(arr, target))
