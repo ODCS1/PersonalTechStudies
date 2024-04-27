cf09d9b523cb59f358793ea3e462d3f7cd2b6536
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-04-27 17:31:26 -0300

    Update ex19.py

--- python/exercicios/lista4/ex19.py
+++ python/exercicios/lista4/ex19.py
@@ -2,24 +2,31 @@
 # dado de um lista de números fornecida.
 
 
-lista = [1, 2, 3, 4, 5, 1, 1, 5]
+lista = [1, 2, 3, 4, 5, 1, 1, 5, 1]
 alvo = 1
-ocorrencias = 0
 
 
-for i in range(len(lista)):
-    if lista[i] == alvo and i < len(lista) - 1:
-        for j in range(i , len(lista)):
-            lista[i] = lista[i+1]
+posicaoAnterior = -1
+encontrou = False
+procurando = True
+
+
+i = len(lista) - 1
+while i > -1:
+    if lista[i] == alvo and procurando:
+        procurando = False
+        encontrou = True
+
+    if encontrou and i == len(lista) - 1:
         lista.pop()
-        ocorrencias += 1
+        encontrou = False
+        procurando = True
+    
+    elif encontrou:
+        posicaoAnterior = i
+        lista[i] = lista[i + 1]
+        i += 2
 
-    if i == len(lista) - 1:
-        pass
-
-
-# for j in range(ocorrencias):
-#     lista.pop()
-
+    i -= 1
 
 print(lista)
