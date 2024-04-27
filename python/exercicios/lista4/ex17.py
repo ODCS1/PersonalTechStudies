03a1eb52fcd88e5a9abebe62bec6888eea0eb601
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-04-26 22:29:11 -0300

    update

--- python/exercicios/lista4/ex17.py
+++ python/exercicios/lista4/ex17.py
@@ -1,2 +1,30 @@
 # 17. Escreva um programa em Python para remover a ocorrência mais à esquerda de um certo 
 # número dado de um lista de números fornecida
+
+
+# FORMA RESUMIDA
+lista = [1, 5, 4, 5, 2 , 1, 8]
+alvo = 1
+
+lista.remove(alvo)
+print(lista)
+
+
+
+# UTILIZANDO LÓGICA PRÓPRIA
+encontrou = False
+contadorParaAlvo = 0
+for i in range(len(lista)):
+    if lista[i] == alvo and contadorParaAlvo == 0:
+        contadorParaAlvo += 1
+        print(contadorParaAlvo)
+        encontrou = True
+
+    if encontrou and i < len(lista) - 1:
+        lista[i] = lista[i+1]
+    
+
+
+lista.pop()
+
+print(lista)
--- python/exercicios/lista4/ex19.py
+++ python/exercicios/lista4/ex19.py
@@ -1,2 +1,25 @@
 # 19. Escreva um programa em Python para remover todas as ocorrências de um certo número 
 # dado de um lista de números fornecida.
+
+
+lista = [1, 2, 3, 4, 5, 1, 1, 5]
+alvo = 1
+ocorrencias = 0
+
+
+for i in range(len(lista)):
+    if lista[i] == alvo and i < len(lista) - 1:
+        for j in range(i , len(lista)):
+            lista[i] = lista[i+1]
+        lista.pop()
+        ocorrencias += 1
+
+    if i == len(lista) - 1:
+        pass
+
+
+# for j in range(ocorrencias):
+#     lista.pop()
+
+
+print(lista)
