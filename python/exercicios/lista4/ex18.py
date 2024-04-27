eae97dec6667fdd146c976c11739208b7478b593
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-04-27 17:03:22 -0300

    Update ex18.py

--- python/exercicios/lista4/ex18.py
+++ python/exercicios/lista4/ex18.py
@@ -2,21 +2,29 @@
 # número dado de um lista de números fornecida
 
 
-lista = [1, 5, 4, 5, 2 , 1, 8]
+lista = [1, 5, 4, 5, 1, 2 , 3, 8]
 alvo = 1
 
 
 encontrou = False
 contadorParaAlvo = 0
 
-for i in range(len(lista)-1, -1, -1):
+
+i = len(lista) - 1
+while i > -1:
     if lista[i] == alvo and contadorParaAlvo == 0:
-        contadorParaAlvo += 1
+        contadorParaAlvo = 1
         encontrou = True
 
-    if encontrou and i < len(lista) - 1:
-        lista[i] = lista[i+1]
+    if encontrou and i == (len(lista) - 1):
         break
+
+    if encontrou:
+        lista[i] = lista[i+1]
+        i += 2
+
+    i -= 1
+        
     
 
 
