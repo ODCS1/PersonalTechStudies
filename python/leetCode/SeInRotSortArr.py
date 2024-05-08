0279b10cb0c21f3e6e24059cb1d1d3517267deb2
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-05-08 01:45:44 -0300

    Update SeInRotSortArr.py

--- python/leetCode/SeInRotSortArr.py
+++ python/leetCode/SeInRotSortArr.py
@@ -4,8 +4,22 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-       
-       pass
+        result = -1
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+
+            if nums[m] == target:
+                result = nums[m]
+                break
+
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+            
+        return result
 
 
 
