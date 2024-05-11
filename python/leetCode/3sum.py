2de12daa88e9cac9b8a376a68b1b5ba931dc9fd1
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-05-11 12:10:46 -0300

    Update 3sum.py

--- python/leetCode/3sum.py
+++ python/leetCode/3sum.py
@@ -2,13 +2,24 @@
 
 from typing import List
 
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
-        pass
+        map = {}
+        l, r = 0, len(nums) - 1
 
+        while l < r:
+            curSum = nums[l] + nums[r]
 
+            if -curSum in map:
+                return [-curSum, l, r]
+            
+            map[nums[l]] = l
+            map[nums[r]] = r
 
+            l += 1
+            r -= 1
 
+arr = [3,4,0,-3]
 obj = Solution()
+print(obj.threeSum(arr))
