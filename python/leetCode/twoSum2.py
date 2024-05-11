a314caa158d3cd78d3e642ea078ee4067438ac07
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-05-11 10:08:35 -0300

    Update twoSum2.py

--- python/leetCode/twoSum2.py
+++ python/leetCode/twoSum2.py
@@ -2,16 +2,35 @@
 
 from typing import List
 
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        map = {}
+# USANDO A LÓGICA ANTERIOR DO TWOSUM 1 -> O(n)
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         map = {}
 
-        for i in range(len(numbers)):
-            dif = target - numbers[i]
-            if dif in map:
-                return [map[dif] + 1, i+1]
+#         for i in range(len(numbers)):
+#             dif = target - numbers[i]
+#             if dif in map:
+#                 return [map[dif] + 1, i+1]
             
-            map[numbers[i]] = i
+#             map[numbers[i]] = i
+
+
+# USANDO POINTERS
+class Solution:
+    def twoSum(self, numbers: List[int], target: int):
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            currentSum = numbers[l] + numbers[r]
+
+            if currentSum > target:
+                r -= 1
+            elif currentSum < target:
+                l += 1
+            else:
+                return [l+1, r+1]
+            
+        return []
 
 
 arr = [1,2,3,4,5,6,7,8]
