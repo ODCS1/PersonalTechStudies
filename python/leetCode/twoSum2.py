c1bfa94e9a6b157947ab74471093063b200f0f18
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-05-11 09:35:55 -0300

    Update twoSum2.py

--- python/leetCode/twoSum2.py
+++ python/leetCode/twoSum2.py
@@ -5,16 +5,13 @@ from typing import List
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         map = {}
-        l, r = 0, len(numbers) - 1
 
-        while l <= r:
-            m = (l + r) // 2
-
-            if target <= numbers[m]:
-                r = m - 1
-            else:
-                
-                pass
+        for i in range(len(numbers)):
+            dif = target - numbers[i]
+            if dif in map:
+                return [map[dif] + 1, i+1]
+            
+            map[numbers[i]] = i
 
 
 arr = [1,2,3,4,5,6,7,8]
