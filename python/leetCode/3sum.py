16ea40ca05aec3d3d06d4181154a690181259ac7
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2024-05-13 00:04:56 -0300

    Update 3sum.py

--- python/leetCode/3sum.py
+++ python/leetCode/3sum.py
@@ -2,24 +2,52 @@
 
 from typing import List
 
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         result = []
+#         map = {}
+#         l, r = 0, len(nums) - 1
+
+#         while l < r:
+#             curSum = nums[l] + nums[r]
+
+#             if -curSum in map:
+#                 return [-curSum, l, r]
+            
+#             map[nums[l]] = l
+#             map[nums[r]] = r
+
+#             l += 1
+#             r -= 1
+
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         result = []
-        map = {}
-        l, r = 0, len(nums) - 1
 
-        while l < r:
-            curSum = nums[l] + nums[r]
+        for i, a in enumerate(nums):
+            if i > 0 and a == nums[i - 1]:
+                continue
 
-            if -curSum in map:
-                return [-curSum, l, r]
-            
-            map[nums[l]] = l
-            map[nums[r]] = r
+            l, r = i + 1, len(nums) - 1
+            curSum = 0
 
-            l += 1
-            r -= 1
+            while l < r:
+                curSum = nums[l] + nums[r] + a
 
-arr = [3,4,0,-3]
+                if curSum > 0:
+                    r -= 1
+                elif curSum < 0:
+                    l += 1
+                else:
+                    result.append([a, nums[l], nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+
+        return result
+
+arr = [-1,0,1,2,-1,-4]
 obj = Solution()
 print(obj.threeSum(arr))
