94d9f155dce1d06cd2aa4d81cd89c42f663d2540
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-05-23 17:27:00 -0300

    up

--- python/exercicios/lista5/ex093.py
+++ python/exercicios/lista5/ex093.py
@@ -1,8 +1,10 @@
 # 93. Escreva uma função em Python que resulte a soma dos elementos de uma dada matriz de 
 # úmeros reais. Faça um programa para testar sua função.
 
-def somaElementosMatriz(matriz):
-    soma = 0
+from typing import List
+
+def somaElementosMatriz(matriz: List[List[int]]) -> int:
+    soma: int = 0
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             soma += matriz[i][j]
@@ -12,13 +14,13 @@ def somaElementosMatriz(matriz):
 
 
 def programa():
-    matrizOriginal = [
+    matrizOriginal: List[int] = [
         [1,2,0,3],
         [6,3,1,0],
         [0,2,0,1]
     ]
 
-    somaElementos = somaElementosMatriz(matrizOriginal)
+    somaElementos: int = somaElementosMatriz(matrizOriginal)
 
     print(f"A soma dos elementos da matriz é igual a: {somaElementos}")
 
