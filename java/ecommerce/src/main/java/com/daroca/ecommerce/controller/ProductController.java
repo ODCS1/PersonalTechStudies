b12b66a2c94101670097c93501fa16ac8fa5ac85
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-06-12 16:45:26 -0300

    update

--- java/ecommerce/src/main/java/com/daroca/ecommerce/controller/ProductController.java
+++ java/ecommerce/src/main/java/com/daroca/ecommerce/controller/ProductController.java
@@ -1,8 +1,14 @@
 package com.daroca.ecommerce.controller;
 
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.swing.text.html.Option;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +21,13 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
+    @GetMapping("/product")
+    public List<Product> all(){
+        return productRepository.findAll();
+    }
+
     @GetMapping("/product/{codigo}")
-    public Product one(){
-        return Product
+    public Optional<Product> one(@PathVariable Integer id){
+        return productRepository.findById(id);
     }
 }
