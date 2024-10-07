86f15a448de33e67a3ae497f4ac511bacf525e35
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2024-10-07 11:37:03 -0300

    Update Recursao.java

--- java/data-structures/exercicios/Recursao.java
+++ java/data-structures/exercicios/Recursao.java
@@ -1,5 +1,5 @@
 public class Recursao {
-   // 1
+    // 1
     public static boolean ehNegativo(int numero) {
         if (numero == 0)
             return false;
@@ -7,8 +7,10 @@ public class Recursao {
     }
 
     public static boolean ehNegativo(int a, int d) {
-        if (a == 0) return true;
-        if (d == 0) return false;
+        if (a == 0)
+            return true;
+        if (d == 0)
+            return false;
         return ehNegativo(++a, --d);
     }
 
@@ -16,8 +18,9 @@ public class Recursao {
     public static int soma(int a, int b) {
         boolean aEhNegativo = ehNegativo(a);
 
-        if (a == 0) return b;
-        
+        if (a == 0)
+            return b;
+
         if (aEhNegativo) {
             return soma(++a, --b);
         }
@@ -28,9 +31,11 @@ public class Recursao {
     // 3
     public static int sub(int a, int b) {
 
-        if (b == 0) return a;
+        if (b == 0)
+            return a;
 
-        if (ehNegativo(b)) return sub(++a, ++b);
+        if (ehNegativo(b))
+            return sub(++a, ++b);
 
         return sub(--a, --b);
     }
@@ -38,7 +43,8 @@ public class Recursao {
     // 4
     public static int modulo(int a) {
 
-        if (ehNegativo(a)) return soma(1, modulo(soma(1, a)));
+        if (ehNegativo(a))
+            return soma(1, modulo(soma(1, a)));
 
         return a;
     }
@@ -46,7 +52,9 @@ public class Recursao {
     // 5
     public static int multi(int a, int b) {
 
-        if (b == 0) { return 0; }
+        if (b == 0) {
+            return 0;
+        }
 
         if (ehNegativo(b)) {
 
@@ -55,30 +63,89 @@ public class Recursao {
 
         return soma(multi(a, sub(b, 1)), a);
     }
-    
-    // 6
-    public static int potenciaComExpoentePositivo(int a, int b) {
-        if (ehNegativo(b)) { potenciaNegativa(a, b); }
 
-        if (b == 0) { return 1; }
-        
+    // 6
+    public static int poten(int a, int b) {
+        if (ehNegativo(b)) {
+            throw new IllegalArgumentException("O expoente não pode ser negativo!!!");
+        }
+
+        if (b == 0) {
+            return 1;
+        }
+
         return multi(poten(a, sub(b, 1)), a);
 
     }
     
-    public static float potenciaComExpoenteNegativo(int a, int b) {
-        
+    // 7
+    public static int div(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("O divisor não pode ser 0!!!");
+        }
 
-        return 0.0;
+        if (ehNegativo(sub(modulo(a), modulo(b)))) {
+            return 0;
+        }
+
+        if (ehNegativo(a)) {
+            if (ehNegativo(b)) {
+                return soma(1, div(sub(a, b), b));
+            }
+
+            return sub(0, div(a, multi(b, -1)));
+        }
+
+        if (ehNegativo(b)) {
+            return sub(0, div(a, multi(b, -1)));
+        }
+
+        return soma(1, div(sub(a, b), b));
+    }
+    
+    // 8
+    public static int resto(int a, int b) {
+
+        return -1;
     }
 
+    // 9
+    public static boolean primeiroEhMaior(int a, int b) {
+
+        return false;
+    }
+    
+    // 10
+    public static boolean primeiroEhMenor(int a, int b) {
+
+        return false;
+    }
+
+    // 11
+    public static boolean teste(int a, int b) {
+
+        return false;
+    }
+
+    // 12
+    public static boolean teste1(int a, int b) {
+
+        return false;
+    }
+    
+    // 13
+    public static boolean teste2(int a, int b) {
+
+        return false;
+    }
+    
     public static void main(String[] args) {
         // System.out.println("Exc 1: " + ehNegativo(5));
         // System.out.println("Exc 2: " + soma(7, 3));
         // System.out.println("Exc 3: " + sub(-5, -3));
         // System.out.println("Exc 4: " + modulo(5));
         // System.out.println("Exc 5: " + multi(-2, -8));
-        System.out.println("Exc 6: " + potenciaComExpoentePositivo(-2, 10));
-        System.out.println(potenciaComExpoenteNegativo(2, -2));
-    } 
+        // System.out.println("Exc 6: " + poten(-2, 3));
+        System.out.println("Exc 7: " + div(10, -5));
+    }
 }
