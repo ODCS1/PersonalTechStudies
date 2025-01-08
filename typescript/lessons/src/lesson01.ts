b70b7c9449525d8547989cff54dd16076ee9e7a8
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-07 22:50:53 -0300

    chore: ts lesson01 and lesson02 adjusts

--- typescript/lessons/src/lesson01.ts
+++ typescript/lessons/src/lesson01.ts
@@ -1,13 +1,14 @@
-let l1_username = 'admin'
-console.log(l1_username)
+let l1_username = "admin";
+console.log(l1_username);
 
-let l1_a: number = 12
-let l1_b: string = "6"
-let l1_c : number= 2
+let l1_a: number = 12;
+let l1_b: string = "6";
+let l1_c: number = 2;
 
-
-if(isNaN(Number(l1_a)) || isNaN(Number(l1_b)) || isNaN(Number(l1_c))){
-    console.log("Impossible to calculate because one of the variables is not a number!")
-}else {
-    console.log(l1_a / Number(l1_b) * 2) 
+if (isNaN(Number(l1_a)) || isNaN(Number(l1_b)) || isNaN(Number(l1_c))) {
+  console.log(
+    "Impossible to calculate because one of the variables is not a number!"
+  );
+} else {
+  console.log((l1_a / Number(l1_b)) * 2);
 }
