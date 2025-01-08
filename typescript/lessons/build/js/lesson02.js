6a4c8aebe7bf7be42ef5e9a0b9f3a11064b11fdb
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-07 22:49:34 -0300

    chore: configs

--- typescript/lessons/build/js/lesson02.js
+++ typescript/lessons/build/js/lesson02.js
@@ -1,28 +1,29 @@
 "use strict";
 // INFERENCE - Typescript verifies the type based on the value assigned to the variable
 // This type is implicit, you can´t change it assigning a new value of a different type to the variable.
-var l2_text1 = "some text to check type";
+let l2_text1 = "some text to check type";
 // EXPLICIT
-var l2_text2 = "Explicit text.";
-var l2_myName = "Prinsk";
-var l2_meaningOfLife;
-var l2_isLoading;
-var l2_album;
-var l2_numberOrString = "text";
+let l2_text2 = "Explicit text.";
+let l2_myName = "Prinsk";
+let l2_meaningOfLife;
+let l2_isLoading;
+let l2_album;
+let l2_numberOrString = "text";
 l2_myName = "Kartorstr";
 l2_meaningOfLife = 92;
 l2_isLoading = true;
 l2_album = "Tasrteskin";
 l2_numberOrString = prompt("l2_numberOrString: ") || "";
-var sum = function (l2_a, l2_b) {
+const sum = (l2_a, l2_b) => {
     return l2_a + l2_b;
 };
 if (!isNaN(Number(l2_numberOrString)) && l2_numberOrString !== "") {
-    console.log("SUM: ".concat(sum(Number(l2_numberOrString), 7)));
+    console.log(`SUM: ${sum(Number(l2_numberOrString), 7)}`);
 }
 else {
     console.log("The current value assignment to the variable is not a number!");
 }
-var postId;
-var isActive;
-var re = /\w+/g;
+// UNION TYPE
+let postId;
+let isActive;
+let re = /\w+/g;
