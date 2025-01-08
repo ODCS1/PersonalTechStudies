0e5e876320b7b18abab83fcbca968ac22701d40c
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-08 18:59:12 -0300

    chore: lesson05-2 JS to TS

---
+++ typescript/lessons/src/lesson05-2.ts
@@ -0,0 +1,11 @@
+// Original JS code
+// const year = document.getElementById("year")
+// const thisYear = new Date().getFullYear()
+// year.setAttribute("datetime", thisYear)
+// year.textContent = thisYear
+
+// MY VERSION TO TS
+const year = document.getElementById("year") as HTMLElement;
+const thisYear = new Date().getFullYear().toString();
+year.setAttribute("datetime", thisYear);
+year.textContent = thisYear;
