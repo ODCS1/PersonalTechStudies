1b2ecc97850dea015e1f7343f9675adfba6ef39d
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-10 21:43:47 -0300

    Create ex01.ts

---
+++ typescript/exercises/ex01.ts
@@ -0,0 +1,6 @@
+// Exercise 1: Basic Types and Variables
+// Objective: Define variables using different basic types and annotations.
+
+let ex1_name: string = "Some Name";
+let ex1_active: string | number | boolean = "archived";
+const ex1_PI: number = 3.14;
