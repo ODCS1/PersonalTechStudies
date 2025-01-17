4295623b4f5789eef451859c1043c8761f6b509d
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-17 00:29:02 -0300

    add: hooks lesson02 app.tsx

--- react/lessons/lesson02/src/App.tsx
+++ react/lessons/lesson02/src/App.tsx
@@ -1,9 +1,17 @@
+import HookCallback from "./components/HookCalback";
+import HookEffect from "./components/HookEffect";
 import Lesson from "./components/Lesson";
+import Multiplication from "./components/Multiplication";
+import Practice from "./components/Practice";
 
 function App() {
   return (
     <>
       <Lesson />
+      <HookEffect />
+      <HookCallback />
+      <Practice />
+      <Multiplication />
     </>
   );
 }
