fa8972b6d2f821c288b15d1215c2117bf3c1088b
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-21 20:46:05 -0300

    feat: components and custom hooks

--- mini-projects/shopping-cart/src/App.tsx
+++ mini-projects/shopping-cart/src/App.tsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import ProductList from "./components/ProductList";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  const [viewCart, setViewCart] = useState(false);
+
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
+    <>
+      <Header viewCart={viewCart} setViewCart={setViewCart}/>
+      {pageContent}
+      <Footer viewCart={viewCart}/>
+    </>
+  );
+
+  
+  return content;
 }
 
 export default App;
