4d09650b2ae92b1c7bb92e6eabfe274ca712ea20
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-22 19:36:02 -0300

    root render

--- react/fundamentals/src/index.jsx
+++ react/fundamentals/src/index.jsx
@@ -1,4 +1,12 @@
-import React from "react";
-import { App } from './App'
+// import React from "react"; // ADDED IN babel.config.js
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+// import { App } from "./App";
 
+// BEFORE REACT 18
+// render(<h1>Hello, World!</h1>, document.getElementById("root"));
 
+// THIS IS CONCURRENT RENDERING IN REACT 18.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
