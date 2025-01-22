0ab0e6ea17c47d485ffc2b1ee333718d6e6112ff
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-22 20:07:02 -0300

    feat: webpack dev server

--- react/fundamentals/webpack.config.js
+++ react/fundamentals/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -10,6 +11,18 @@ module.exports = {
   resolve: {
     extensions: [".js", ".jsx"],
   },
+  devServer: {
+    // BEFORE WebPack Dev Server 5
+    // contentBase: path.resolve(__dirname, 'public'),
+    static: {
+      directory: path.join(__dirname, 'dist')
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
