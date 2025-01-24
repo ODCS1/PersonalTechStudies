ac828b5165deb9a81b60027ab3e23736f9e6a6e5
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-24 16:46:03 -0300

    feat: global style
    Added the styled-components dependency

---
+++ mini-projects/onmoney/src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+    :root {
+        --background: #f0f2ff;
+        --red: #e52e4d;
+        --blue: #5429CC;
+
+        --blue-light: #6933FF;
+
+        --text-title: #363F5F;
+        --text-body: #969cb3;
+
+        --backgorund: #F0F2F5;
+        --shape: #FFFFFF;
+    }
+
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    // font-size: 16px (desktop)
+    html {
+        @media (max-width: 1080){
+            font-size: 93.75%; // 15px
+        }
+
+        @media (max-width: 720px){
+            font-size: 87.5%; // 14px
+        }
+    }
+
+    // REM = 1rem = 16px
+
+    body {
+        background: var(--background);
+
+        // SHARPER FONTS
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`;
