96c34cf2db7f1cc87cde47704ee517956935f4b7
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-24 11:53:33 -0300

    fix: webpack (j|t)sx regex

---
+++ react/fundamentals/webpack.test.config.js
@@ -0,0 +1,5 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpackConfig = require("./webpack.config.js");
+
+describe("Webpack Configuration", () => {});
