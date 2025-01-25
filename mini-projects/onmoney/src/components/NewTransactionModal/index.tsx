ba97187fabf06c586bf90c0be40054a8a53a76ce
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-25 17:58:03 -0300

    feat: modal update

--- mini-projects/onmoney/src/components/NewTransactionModal/index.tsx
+++ mini-projects/onmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,9 @@
 import Modal from "react-modal";
+import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import closeSvg from "../../assets/close.svg";
+import incomeSvg from "../../assets/income.svg";
+import outcomeSvg from "../../assets/outcome.svg";
+import { useState } from "react";
 
 Modal.setAppElement("#root");
 
@@ -9,13 +14,46 @@ type newTransactionModalProps = {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose }: newTransactionModalProps) {
+  const [type, setType] = useState('deposit');
 
   return (
     <Modal
         isOpen={isOpen}
         onRequestClose={onRequestClose}
+        overlayClassName={"react-modal-overlay"}
+        className={"react-modal-content"}
     >
-        <h2>Register Transaction</h2>
+        <button type="button" className="react-modal-close">
+            <img src={closeSvg} alt="Close modal" onClick={onRequestClose} />
+        </button>
+        <Container>
+          <h2>Register Transaction</h2>
+          <input placeholder="Title"/>
+          <input type="number" placeholder="Value" />
+
+          <TransactionTypeContainer>
+            <RadioBox
+              isActive={type === 'deposit'}
+              type="button"
+              onClick={() => { setType('deposit'); }}
+            >
+              <img src={incomeSvg} alt="Income" />
+              <span>Income</span>
+            </RadioBox>
+
+            <RadioBox
+              isActive={type === 'withdraw'}
+              type="button"
+              onClick={() => { setType('withdraw'); }}
+            >
+              <img src={outcomeSvg} alt="Outcome" />
+              <span>Outcome</span>
+            </RadioBox>
+          </TransactionTypeContainer>
+
+          <input placeholder="Category" />
+          <button type="submit">Register</button>
+        </Container>
     </Modal>
   );
 };
