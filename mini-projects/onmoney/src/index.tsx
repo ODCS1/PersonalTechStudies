dbe1975ec44d6933ded87da95bb0a6a78b201734
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-25 08:56:12 -0300

    feat: api fetch using miragejs

--- mini-projects/onmoney/src/index.tsx
+++ mini-projects/onmoney/src/index.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Website development',
+          type: 'deposit',
+          category: 'Sale',
+          amount: 12000,
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Apartment rental',
+          type: 'withdraw',
+          category: 'Home',
+          amount: 12000,
+          createdAt: new Date()
+        }
+      ];
+    });
+  }
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
