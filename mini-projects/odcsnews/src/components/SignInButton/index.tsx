f74c8bfe7eb67119d37894dfd36a611521c21b17
Antonio Ilton <119748470+ODCS1@users.noreply.github.com>
2025-01-29 20:27:04 -0300

    chore: SignInButon

---
+++ mini-projects/odcsnews/src/components/SignInButton/index.tsx
@@ -0,0 +1,10 @@
+// import style from './style.module.scss';
+// import { FaGithub } from 'react-icons/fa';
+
+export const SignInButton = () => {
+  return (
+    <button type="button">
+
+    </button>
+  )
+}
